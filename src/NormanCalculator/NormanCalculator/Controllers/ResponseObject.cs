3189bb8026589fd2d27b59b2246ef7af10a219bb
olusola-adio <olusola_adio@hotmail.com>
2021-02-21 18:44:42 +0000

    initial-commit

---
+++ src/NormanCalculator/NormanCalculator/Controllers/ResponseObject.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace NormanCalculator.Controllers
+{
+    public class ResponseObject
+    {
+        [JsonProperty("result")]
+        public string Result { get; set; }
+    }
+}
---
+++ src/NormanCalculator/NormanCalculator/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace NormanCalculator
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "NormanCalculator API",
+                    Description = "Implementation of NormanCalculator in ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Norman And Sons",
+                        Email = "olusola.adio@normanandsons.com",
+                        Url = "http://www.normanandsons.com"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "http://www.normanandsons.com"
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "NormanCalculator ASP.NET Core Web API");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseMvc();
+        }
+    }
+}
